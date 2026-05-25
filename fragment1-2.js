ff7bbfb27a1d37d681c3c93d19eb0302aa5c60ec
Flo <flodufnaf3@gmail.com>
2026-05-26 00:28:58 +0200

    Update fragment1-2.js

--- fragment1-2.js
+++ fragment1-2.js
@@ -34,3 +34,60 @@ function validate() {
     error.innerText = "ECHEC DE DECODAGE";
   }
 }
+
+const phrase = "L17 OBSERVEUR NON AUTORISE";
+
+// exemple texte décodé (lore)
+const loreText = `
+L’observation a commencé avant la mise en place du protocole.
+Certains éléments ne devraient pas être visibles.
+
+Le système L17 conserve des fragments dispersés dans les couches mémoire.
+Aucune confirmation officielle n’existe.
+
+Nous avons identifié une anomalie dans la séquence centrale.
+Elle ne répond plus aux règles de classification.
+
+Toute tentative de suppression entraîne une réaction du système.
+`;
+
+function validate() {
+  const input = document.getElementById("input").value.trim().toUpperCase();
+  const error = document.getElementById("error");
+
+  if (input === phrase) {
+
+    document.getElementById("cipherText").style.display = "none";
+    document.getElementById("input").style.display = "none";
+    document.querySelector("button").style.display = "none";
+
+    error.style.color = "#6CFF6C";
+    error.innerText = "DÉCRYPTAGE RÉUSSI";
+
+    setTimeout(() => {
+      showLore();
+    }, 800);
+
+  } else {
+    error.style.color = "#FF4C4C";
+    error.innerText = "ÉCHEC DE DÉCODAGE";
+  }
+}
+
+function showLore() {
+  const container = document.querySelector(".card");
+
+  const loreDiv = document.createElement("div");
+  loreDiv.className = "lore";
+
+  loreDiv.innerHTML = `
+    <p class="lore-text">${loreText}</p>
+
+    <p class="hidden-link">
+      https://flodufnaf.github.io/experience.github.io/
+      <span class="glitch-url">XXXXXXXX</span>
+    </p>
+  `;
+
+  container.appendChild(loreDiv);
+}

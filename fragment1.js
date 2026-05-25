545faa0a2d3c0c1b7b4fed223d57aafc5725ef57
Flo <flodufnaf3@gmail.com>
2026-05-25 21:30:11 +0200

    Update fragment1.js

--- fragment1.js
+++ fragment1.js
@@ -9,51 +9,14 @@ timerEl.innerText = "Temps restant : " + timeLeft + "s";
 
 const interval = setInterval(() => {
   timeLeft--;
+
   timerEl.innerText = "Temps restant : " + timeLeft + "s";
 
   if (timeLeft <= 0) {
     clearInterval(interval);
     window.location.href = "index.html";
   }
-}, 1000);
 
-function validateCaptcha() {
-  const input = document.getElementById("captchaInput").value;
-  const error = document.getElementById("error");
-
-  if (input === phrase) {
-    clearInterval(interval);
-    error.style.color = "#6CFF6C";
-    error.innerText = "VALIDÉ";
-
-    setTimeout(() => {
-      window.location.href = "home.html";
-    }, 800);
-
-  } else {
-    error.style.color = "#FF4C4C";
-    error.innerText = "INTRUSION NON VALIDÉE";
-  }
-}
-
-const phrase = "L17 OBSERVEUR NON AUTORISÉ";
-
-// affichage progressif (optionnel mais stylé)
-document.getElementById("captchaText").innerText = phrase;
-
-let timeLeft = 10;
-
-const timerEl = document.getElementById("timer");
-timerEl.innerText = "Temps restant : " + timeLeft + "s";
-
-const interval = setInterval(() => {
-  timeLeft--;
-  timerEl.innerText = "Temps restant : " + timeLeft + "s";
-
-  if (timeLeft <= 0) {
-    clearInterval(interval);
-    window.location.href = "index.html";
-  }
 }, 1000);
 
 function normalize(str) {
@@ -64,10 +27,12 @@ function normalize(str) {
 }
 
 function validateCaptcha() {
+
   const input = document.getElementById("captchaInput").value;
   const error = document.getElementById("error");
 
   if (normalize(input) === normalize(phrase)) {
+
     clearInterval(interval);
 
     error.style.color = "#6CFF6C";
@@ -78,7 +43,9 @@ function validateCaptcha() {
     }, 800);
 
   } else {
+
     error.style.color = "#FF4C4C";
     error.innerText = "INTRUSION NON VALIDÉE";
+
   }
 }

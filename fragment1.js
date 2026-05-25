122bfa72012df0fde4f0c81f3322f2ddb78df628
Flo <flodufnaf3@gmail.com>
2026-05-25 21:11:45 +0200

    Create fragment1.js

---
+++ fragment1.js
@@ -0,0 +1,37 @@
+const phrase = "L17 OBSERVEUR NON AUTORISÉ";
+
+document.getElementById("captchaText").innerText = phrase;
+
+let timeLeft = 10;
+
+const timerEl = document.getElementById("timer");
+timerEl.innerText = "Temps restant : " + timeLeft + "s";
+
+const interval = setInterval(() => {
+  timeLeft--;
+  timerEl.innerText = "Temps restant : " + timeLeft + "s";
+
+  if (timeLeft <= 0) {
+    clearInterval(interval);
+    window.location.href = "index.html";
+  }
+}, 1000);
+
+function validateCaptcha() {
+  const input = document.getElementById("captchaInput").value;
+  const error = document.getElementById("error");
+
+  if (input === phrase) {
+    clearInterval(interval);
+    error.style.color = "#6CFF6C";
+    error.innerText = "VALIDÉ";
+
+    setTimeout(() => {
+      window.location.href = "home.html";
+    }, 800);
+
+  } else {
+    error.style.color = "#FF4C4C";
+    error.innerText = "INTRUSION NON VALIDÉE";
+  }
+}

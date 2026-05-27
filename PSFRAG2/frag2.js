c46f8abd5cf621fbfc05197e0eba5d1071d03afb
Flo <flodufnaf3@gmail.com>
2026-05-27 22:45:53 +0200

    Update frag2.js

--- PSFRAG2/frag2.js
+++ PSFRAG2/frag2.js
@@ -1,36 +1,10 @@
-let time = 8;
-const timerEl = document.getElementById("timer");
-
-const interval = setInterval(() => {
-  time--;
-
-  if (time >= 10) {
-    timerEl.innerText = "00:" + time;
-  } else {
-    timerEl.innerText = "00:0" + time;
-  }
-
-  if (time <= 0) {
-    clearInterval(interval);
-
-    // TIMEOUT → retour accueil
-    window.location.href = "https://flodufnaf.github.io/experience.github.io/accueil/";
-  }
-}, 1000);
-
-function validate(correct) {
+function validate() {
   const result = document.getElementById("result");
 
-  if (correct) {
-    result.innerText = "VALIDATION ACCEPTÉE";
-    result.style.color = "#6CFF6C";
+  result.innerText = "VALIDATION ACCEPTÉE";
+  result.style.color = "#6CFF6C";
 
-    setTimeout(() => {
-      window.location.href = "https://flodufnaf.github.io/experience.github.io/PSFRAG2/home";
-    }, 600);
-
-  } else {
-    result.innerText = "CORRUPTION DÉTECTÉE";
-    result.style.color = "#FF4C4C";
-  }
+  setTimeout(() => {
+    window.location.href = "https://flodufnaf.github.io/experience.github.io/PSFRAG2/home";
+  }, 500);
 }
